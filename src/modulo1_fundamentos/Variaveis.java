bec958259468ad9fe2371992b1562e36b2d652f7
Marx dos Santos <marx.softwaredeveloper@gmail.com>
2025-01-03 22:59:47 -0300

    Commit Code Variaveis

---
+++ src/modulo1_fundamentos/Variaveis.java
@@ -0,0 +1,38 @@
+package modulo1_fundamentos;
+
+public class Variaveis {
+
+	public static void main(String[] args) {
+		System.out.println("=== VARIÁVEIS ===");
+		System.out.println();
+		
+		// DECLARANDO UMA VARIÁVEL E DEPOIS INICIALIZANDO
+		int numero; // Declaração
+		numero = 29; // Inicialização
+		System.out.println("Número: " + numero);
+		System.out.println();
+		
+		
+		// DECLARANDO E INICIALIZANDO SIMULTANEAMENTE 
+		String nome = "Marx";
+		System.out.println("Meu nome é " + nome);
+		System.out.println();
+		
+		
+		// DECLARANDO MULTIPLAS VARIÁVEIS
+		int valor1 = 10, valor2 = 20, valor3 = 30;
+		System.out.println();
+		
+		
+		// PROCESSANDO DADOS COM VARIÁVEIS
+		int soma = valor1 + valor2 + valor3;
+		System.out.println(soma);
+		System.out.println();
+		
+		
+		//TIPOS DE VARIÁVEIS
+		System.out.println("=== TIPOS DE VARIÁVEIS ===");
+		System.out.println("Variáveis Locais");
+	}
+
+}
