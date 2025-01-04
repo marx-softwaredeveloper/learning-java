4f87c65f54ad8d9ff25a6499f7e5a95992ac6f91
Marx dos Santos <marx.softwaredeveloper@gmail.com>
2025-01-03 23:00:13 -0300

    Commit Code Estrutura Basica

---
+++ src/modulo1_fundamentos/EstruturaBasica.java
@@ -0,0 +1,17 @@
+// Declaração do pacote
+package modulo1_fundamentos;
+
+//Declaração da classe principal
+public class EstruturaBasica {
+
+	// Método principal: ponto de entrada do programa
+	public static void main(String[] args) {
+		//public: Torna o método acessível fora da classe.
+		//static: Permite que o método seja chamado sem criar um objeto da classe.
+		//void: Indica que o método não retorna valor.
+		//String[] args: Representa os argumentos passados pela linha de comando.
+		
+		// Corpo do programa
+		System.out.println("My first program!");
+	}
+}
